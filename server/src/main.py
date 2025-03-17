5b4d8774821504fb6c4543d7e498154e1f8d2d7c
SamuraiJeka <offissit123321@gmail.com>
2025-03-17 15:59:07 +0300

    Create docker-compose

--- server/src/main.py
+++ server/src/main.py
@@ -6,8 +6,3 @@ import routers.anecdote as anecdote
 app = FastAPI()
 
 app.include_router(anecdote.router)
-
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run("main:app", reload=False)
