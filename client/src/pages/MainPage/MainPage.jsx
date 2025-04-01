dc56d60b1ab01a621c59569ebf331809d5e8867a
SamuraiJeka <offissit123321@gmail.com>
2025-04-01 15:12:49 +0300

    Naverstal govna

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MainPage.module.scss"
 import LinkPage from "../../shared/Link/LinkPage";
 const MainPage = () =>
 {
+    const get_random_anecdote = () => {
+        const response = axios.get("localhost:5000/anecdote/random")
+        return response.data
+    }
+
     return(
-        <div>
-            
+        <div className={styles.main}>
+            <input type="text"/>
+            <div className={styles.anecdote}>
+                <h1>Название</h1>
+                <h4>fefef</h4>
+            </div>
+            <div>
+                <button className={styles.random_button} onClick={() => get_random_anecdote()}>Рандомный анекдот</button>
+            </div>
         </div>
-        
     )
 }
 
