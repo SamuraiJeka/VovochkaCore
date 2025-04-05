c236a240919fb85f13f2f60edeba93b7f4501d83
SamuraiJeka <offissit123321@gmail.com>
2025-04-06 00:05:10 +0300

    Create views for admin panel

--- server/src/main.py
+++ server/src/main.py
@@ -4,6 +4,7 @@ from sqladmin import Admin
 
 from core.db import engine, settings
 from utils.admin.auth import AdminAuth 
+from utils.admin.views import AdminView, AnecdoteView
 
 import routers.anecdote_router as anecdote
 
@@ -16,6 +17,8 @@ admin = Admin(
     engine=engine,
     authentication_backend=authentication_backend
 )
+admin.add_view(AdminView)
+admin.add_view(AnecdoteView)
 
 app.add_middleware(
     CORSMiddleware,
